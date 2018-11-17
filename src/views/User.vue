fefb0fe0da6a3b76e9b6a8bf6b56b86fb7ed0176
kball <kball@zendev.com>
2018-11-17 14:32:37 -0800

    Basic user view and route

---
+++ src/views/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>This is for User {{id}}</h1>
+
+    <router-link :to="nextUserUrl">Next User</router-link>
+  </div>
+</template>
+
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
+    nextUserUrl() {
+      return this.id + 1;
+    },
+  },
+  watch: {
+    $route() {
+      console.log('new route', this.$route); //eslint-disable-line no-console
+    },
+  },
+  mounted() {
+    console.log(this.$route); //eslint-disable-line no-console
+  },
+}
+</script>
